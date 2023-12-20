d0f5221fbe0dc15348b24807dd72260546a27705
ailtonlopesmendes <ailtonlopesmendes@ippon.fr>
2023-12-20 09:02:03 +0100

    [#1] feat: create PicOs project

---
+++ PicOs/PicOsApp.swift
@@ -0,0 +1,17 @@
+//
+//  PicOsApp.swift
+//  PicOs
+//
+//  Created by ailton lopes mendes on 19/12/2023.
+//
+
+import SwiftUI
+
+@main
+struct PicOsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
